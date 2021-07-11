f6a1c048ccd0e47e2e1c91a2379cf58e93101537
Glenks123 <63196484+Glenks123@users.noreply.github.com>
2021-07-11 19:34:45 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,14 @@
+import firebase from 'firebase';
 
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID',
+  appId: process.env.APP_ID,
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const db = firebaseApp.firestore();
+export const auth = firebase.auth();
